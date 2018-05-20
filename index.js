c1122643290810fd240fc1080cf10663398e6b99
ProUbeo <38327240+ProUbeo@users.noreply.github.com>
2018-05-20 09:30:01 +0200

    Update index.js

--- index.js
+++ index.js
@@ -25,5 +25,19 @@ bot.on('message', message => {
     
         message.channel.send({embed});
     }
+    if(message.content === "rp!test" ){ 
+
+    const embed = new Discord.RichEmbed()
+    .setTitle("Test d'image")
+    .setAuthor("RpBot")
+    .setColor(0xD4FE00)
+    .setDescription("test")
+    .setFooter("si vous avez un problème... index.js ne répond pas")
+          .addField("test du lien",
+      "donnée test")
+      .setImage("https://d2v9y0dukr6mq2.cloudfront.net/video/thumbnail/EGy7pjQwl/multicolor-video-with-text-error-404-not-found_skwt525re_thumbnail-small01.jpg")
+  
+    message.channel.send({embed});
+}
 }
 )
