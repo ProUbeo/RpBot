52125b708519a3e58d32509420acbde354e02ea5
ProUbeo <38327240+ProUbeo@users.noreply.github.com>
2018-05-21 12:45:55 +0200

    Update index.js

--- index.js
+++ index.js
@@ -21,7 +21,7 @@
          .setDescription("")
          . setFooter ( "Si vous avez un problème ... index.js ne répond pas" )
          . addField ( "liste",
-           "```help1.exe                                              _ ☐ X\nfaire apparaître des armes :\nrp!katana pour faire apparaître un katana (la logique)\nrp!hache pour faire apparaître attention suspens... une hache (bon ok j'arrète)\nrp!pour faire apparaître une canne a pêche... et non c'est pour un arc XD```" )
+           "```help1.exe                                              _ ☐ X\nfaire apparaître des armes :\nrp!katana pour faire apparaître un katana (la logique)\nrp!hache pour faire apparaître attention suspens... une hache (bon ok j'arrète)\nrp!arc pour faire apparaître une canne a pêche... et non c'est pour un arc XD\nrp!faux poir faire apparaître une Faux et pas de Norman, merci :3```" )
     
          message.channel.send({embed});
      }
@@ -51,6 +51,11 @@ if(message.content.startsWith("rp!hache")){
 if(message.content.startsWith("rp!arc")){
     message.channel.send(`**${message.author.username}** fait apparaître son arc !`,{
     file:"http://img4.hostingpics.net/pics/484588arc.png"
+})
+}
+if(message.content.startsWith("rp!faux")){
+    message.channel.send(`**${message.author.username}** fait apparaître sa Faux !`,{
+    file:"https://cdn.discordapp.com/attachments/448046019301736452/448060981978071040/th.jpg"
 })
 }
 }
