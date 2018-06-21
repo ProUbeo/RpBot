d0a8eab16502d22289d6db05d7cc232be131257d
ProUbeo <38327240+ProUbeo@users.noreply.github.com>
2018-06-21 19:03:42 +0200

    Update index.js

--- index.js
+++ index.js
@@ -193,7 +193,7 @@ if (message.content.startsWith("rp!annonce")){
     .addField("[Important, Annonce]", thingToEcho)
     .setColor(0xD4FE00)
     .setFooter("si vous avez un problème... index.js ne répond pas")
-    message.guild.channels.find("<#445976354119483392>").sendEmbed(embed)
+    message.channel.sendEmbed(embed)
     .then(function (message) {
 message.react("✔️")    
     }).catch(function() {
