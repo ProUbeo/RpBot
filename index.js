c4214e3f8370a725c11a0edd6ffab30b9c283801
ProUbeo <38327240+ProUbeo@users.noreply.github.com>
2018-12-03 19:23:08 +0100

    Update index.js

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ const Discord = require('discord.js');
  
  bot.on("guildMemberAdd", member => {
     
-member.guild.channels.find("name", "entrer").send(`${member}, je te souhaite la bienvenue ! Je t'offre le pouvoir du rp (et pour l'utiliser... ah ! Tu connais pas ! Fait donc "rp!help" :3`)
+member.guild.channels.find("name", "entrer-hrp").send(`${member}, je te souhaite la bienvenue ! Je t'offre le pouvoir du rp (et pour l'utiliser... ah ! Tu connais pas ! Fait donc "rp!help" :3`)
     })
     bot.on("guildMemberRemove", member => {
     member.guild.channels.find("name", "entrer").send(`${member} n'a plus le pouvoir du RP`)
